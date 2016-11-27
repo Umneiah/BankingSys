d09217da9f8cdb94db64c2d219fff2af10ef9834
mahmoudhablas <mahmoudhablas263@yahoo.com>
2016-11-27 16:25:12 +0200

    Update BankingSystem.java

--- src/bankingsystem/BankingSystem.java
+++ src/bankingsystem/BankingSystem.java
@@ -9,33 +9,59 @@ package bankingsystem;
  *
  * @author mostafa
  */
-public class BankingSystem {
+import java.sql.*;
+import java.io.*;
+import java.util.*;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.*;
+public ClientHandler extends Thread{
+         Socket c;
+    Connection myconn = null;
+    Statement mystat = null;
+    ResultSet myres = null;
+    public   ClientHandler (Socket c) {
+        this.c = c;
+        try{
+            myconn = DriverManager.getConnection("jdbc:mysql://localhost:3306/bankingsystem","root","");
+            mystat = myconn.createStatement();
+        }catch(Exception f)
+        {
+            
+        }
+         public void run() {
 
-    /**
-     * @param args the command line arguments
-     */
-    
-    public static void Register()
-    {
-        
+        try {
+            DataInputStream dis
+                    = new DataInputStream(c.getInputStream());
+            DataOutputStream dos
+                    = new DataOutputStream(c.getOutputStream());
+                String s = dis.readUTF();
+                String p = dis.readUTF();
+                String amount = checkVaild(s,p);
+                
+                dos.writeUTF(amount);
+                
+                String a = dis.readUTF();
+                String withdraw_or_deposite = dis.readUTF();
+                String h ="";
+                if(withdraw_or_deposite.equals("w"))
+                {
+                   h = withdraw(a,s);
+                }
+                dos.writeUTF(h);
+               c.close();
+
+        } 
+        catch (Exception e) 
+        {
+            System.out.println("Something went wrong");
+        }
+}
     }
-    public static void Login()
-    {
-        
-    }
-    public static void Deposit()
-    {
-        
-    }
-    public static void Withdraw()
-    {
-        
-    }
-    public static void GetTransHistory()
-    {
-        
-    }
-    
+}
+public class BankingSystem{
+   
     public static void main(String[] args) {
         // TODO code application logic here
     }
