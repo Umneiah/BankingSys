349d0ea22f568936cc96e16db2267f9dcd69e77a
abuali880 <future.engineer77@gmail.com>
2016-11-27 16:19:41 +0200

    functions

--- src/bankingsystem/BankingSystem.java
+++ src/bankingsystem/BankingSystem.java
@@ -14,6 +14,28 @@ public class BankingSystem {
     /**
      * @param args the command line arguments
      */
+    
+    public static void Register()
+    {
+        
+    }
+    public static void Login()
+    {
+        
+    }
+    public static void Deposit()
+    {
+        
+    }
+    public static void Withdraw()
+    {
+        
+    }
+    public static void GetTransHistory()
+    {
+        
+    }
+    
     public static void main(String[] args) {
         // TODO code application logic here
     }
